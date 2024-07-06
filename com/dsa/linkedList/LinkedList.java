e2b3a7a45237b69815f9872dfb57e0e9314c5509
Hrithik Raj <hrithikraj70@gmail.com>
2024-07-06 20:38:24 +0530

    [Hrithik] : Added Print,Size and Middle Element - LinkedList.

--- com/dsa/linkedList/LinkedList.java
+++ com/dsa/linkedList/LinkedList.java
@@ -1,11 +1,13 @@
 package com.dsa.linkedList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LinkedList {
 
     Node headNode;
 
-    public void insertAtBeginning(Object data)
-    {
+    public void insertAtBeginning(Object data) {
         Node newNode = new Node(data);
         newNode.nextNode = headNode;
         headNode = newNode;
@@ -24,15 +26,40 @@ public class LinkedList {
         }
     }
 
-    public void printLinkedList()
-    {
-        Node currentNode =headNode;
-        while (currentNode != null)
-        {
+    public void printLinkedList() {
+        Node currentNode = headNode;
+        while (currentNode != null) {
             System.out.print(currentNode.data + "->");
             currentNode = currentNode.nextNode;
         }
         System.out.print("null");
         System.out.println();
+    }
+
+    public void getSize() {
+        int size = 0;
+        Node newNode = headNode;
+        while (newNode != null) {
+            size++;
+            newNode = newNode.nextNode;
+        }
+        System.out.println("Size : " + size);
+        ;
+    }
+
+    public void getMiddleElement() {
+        Node currentNode = headNode;
+        List<Object> list = new ArrayList<Object>();
+        while (currentNode != null) {
+            list.add(currentNode.data);
+            currentNode = currentNode.nextNode;
+        }
+        if (list.isEmpty())
+            System.out.println("The list is empty.");
+        else {
+            int middleIndex = list.size() / 2;
+            Object middleElement = list.get(middleIndex);
+            System.out.println("Middle Element : ["+middleElement+"]");
+        }
     }
 }
