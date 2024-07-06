5a2670c9175fc22f7d74842bfc42aa4c5822e458
Hrithik Raj <hrithikraj70@gmail.com>
2024-07-06 22:06:46 +0530

    [Hrithik] : Added Insertion at Beginning.

--- com/dsa/question/LinkedList/InsertionBeginning.java
+++ com/dsa/question/LinkedList/InsertionBeginning.java
@@ -1,4 +1,35 @@
-package com.dsa.question.LinkedList;
+package com.dsa.Question.LinkedList;
 
 public class InsertionBeginning {
+
+    Node head;
+
+    public void insertAtBeginning(Object data)
+    {
+        Node newNode = new Node(data);
+        newNode.nextNode =head;
+        head =newNode;
+    }
+    public void printList()
+    {
+        Node currentNode = head;
+        while (currentNode != null)
+        {
+            System.out.print(currentNode.data + "->");
+            currentNode = currentNode.nextNode;
+        }
+        System.out.print("null");
+    }
+
+    public static void main(String[] args) {
+        InsertionBeginning insertionBeginning = new InsertionBeginning();
+        insertionBeginning.insertAtBeginning(1);
+        insertionBeginning.insertAtBeginning(2);
+        insertionBeginning.insertAtBeginning(3);
+        insertionBeginning.insertAtBeginning(4);
+
+        insertionBeginning.printList(); // 4->3->2->1->null
+    }
+
+
 }
---
+++ com/dsa/question/LinkedList/Node.java
@@ -0,0 +1,11 @@
+package com.dsa.Question.LinkedList;
+
+public class Node {
+    Object data;
+    Node nextNode;
+
+    Node(Object nodeData)
+    {
+        this.data = nodeData;
+    }
+}
