8c21ea5033663329be2f982fc391a01e517a4f0d
Hrithik Raj <hrithikraj70@gmail.com>
2024-07-06 19:14:45 +0530

    [Hrithik] : Added Insert Beg & Last - LinkedList.

---
+++ com/hrithik/dsa/Main.java
@@ -0,0 +1,21 @@
+package com.hrithik.dsa;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.insertAtBeginning(1);
+        linkedList.insertAtBeginning("Hrithik Raj");
+        linkedList.insertAtBeginning(true);
+        linkedList.insertAtBeginning(3.84);
+        linkedList.insertAtBeginning("Java");
+
+        System.out.println("-----------Insertion At Beginning ----------");
+        linkedList.printLinkedList(); // Java->3.84->true->Hrithik Raj->1->null
+
+        System.out.println("-----------Insertion At Last ----------");
+        linkedList.insertAtLast("Last Element");
+        linkedList.printLinkedList(); // Java->3.84->true->Hrithik Raj->1->Last Element->null
+
+
+    }
+}
---
+++ com/hrithik/dsa/Node.java
@@ -0,0 +1,12 @@
+package com.hrithik.dsa;
+
+public class Node {
+    Object data;
+    Node nextNode;
+
+    Node(Object data)
+    {
+        this.data =data;
+        this.nextNode = null;
+    }
+}
